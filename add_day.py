b0cfbce65510b6cfc3722bca6ed8fa58985f92a0
David Gildour <davidgildour@gmail.com>
2025-04-10 22:33:28 +0200

    added code template generation for golang

--- add_day.py
+++ add_day.py
@@ -3,33 +3,6 @@ import argparse
 import os
 from pathlib import Path
 
-TEMPLATE = """\"\"\"https://adventofcode.com/{year}/day/{day}\"\"\"
-import argparse
-
-
-def part_one(data: list[str]):
-    ...
-
-
-def part_two(data: list[str]):
-    ...
-
-
-parser = argparse.ArgumentParser(description="Solution for Advent of Code {day}/{year}.")
-parser.add_argument("-t", "--test", action="store_true", help="use test input")
-
-if __name__ == "__main__":
-    args = parser.parse_args()
-    filename = "./input_test.txt" if args.test else "./input.txt"
-
-    with open(filename) as f:
-        raw_data = [x.strip() for x in f.readlines()]
-
-    first_answer = part_one(raw_data)
-    print(f"PART ONE: The answer to part one is equal to {{first_answer}}.")
-    second_answer = part_two(raw_data)
-    print(f"PART TWO: The answer to part two is equal to {{second_answer}}.")
-"""
 
 parser = argparse.ArgumentParser(
     description="Create a boilerplate code and directory structure (in a current directory!) for a challenge on"
@@ -37,32 +10,31 @@ parser = argparse.ArgumentParser(
 )
 parser.add_argument("year", type=int, help="a day on the AoC")
 parser.add_argument("day", type=int, help="a year of the AoC")
+parser.add_argument("-l", "--lang", type=str, default="py", choices=["py", "go"], help="a programming language to generate a template for")
 
 
-def create_boilerplate(year: int, day: int):
+def create_boilerplate(year: int, day: int, language: str) -> str:
     base = Path() / str(year) / str(day)
     os.makedirs(base, exist_ok=True)
 
-    solution = base / "solution.py"
+    solution = base / f"solution.{language}"
     input_test = base / "input_test.txt"
     input_proper = base / "input.txt"
-    try:
-        solution.touch(exist_ok=False)
-        input_test.touch(exist_ok=False)
-        input_proper.touch(exist_ok=False)
-    except FileExistsError as e:
-        return (
-            f"Failed to create boilerplate, are you sure you typed the new day instead of an existing one?\n"
-            f"ERROR: {e}"
-        )
 
-    with open(solution, "w") as f:
-        f.write(TEMPLATE.format(year=year, day=day))
+    if os.path.exists(solution):
+        return f"Boilerplate ({language}) for AoC {year}/{day} already exists."
+    solution.touch(exist_ok=True)
+    input_test.touch(exist_ok=True)
+    input_proper.touch(exist_ok=True)
 
-    return f"Created boilerplate for AoC {year}/{day}."
+    with open(solution, "w") as output_file, open(f"templates/{language}.template") as template_file:
+        template_str = template_file.read()
+        output_file.write(template_str.format(year=year, day=day))
+
+    return f"Created ({language}) boilerplate for AoC {year}/{day}."
 
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    message = create_boilerplate(args.year, args.day)
+    message = create_boilerplate(args.year, args.day, args.lang)
     print(message)
